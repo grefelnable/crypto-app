6f2669b9a8e9f71346fcff6cf23b86e5b817896a
Grefel Nable <felnable@gmail.com>
2023-04-06 02:22:14 -0400

    Finish Error Page Layout

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -7,7 +7,9 @@ const Error = () => {
       <div>
         <h2>404</h2>
         <p>Page not Found</p>
-        <Link to="/">Back Home</Link>
+        <Link to="/">
+          <span>Back Home</span>{" "}
+        </Link>
       </div>
     </Section>
   );
@@ -20,8 +22,17 @@ const Section = styled.section`
   height: 100vh;
   display: grid;
   place-items: center;
+  text-align: center;
 
   h2 {
     color: var(--primary-500);
+    font-weight: 500;
+    font-size: 10rem;
+  }
+  span {
+    color: var(--primary-500);
+  }
+  span:hover {
+    color: var(--primary-300);
   }
 `;

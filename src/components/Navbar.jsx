53d6e1d608331d9f48e3a7361004a6fe4ccc5a63
Grefel Nable <felnable@gmail.com>
2023-04-08 08:40:00 -0400

    Style Search bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import logo from "../assets/crypto-logo.png";
+import { ReactComponent as SearchIcon } from "../assets/search-icon.svg";
 
 const Navbar = () => {
   return (
@@ -7,6 +8,12 @@ const Navbar = () => {
       <LogoContainer>
         <img src={logo} alt="crypto logo by grefel" className="img" />
       </LogoContainer>
+      <SearchContainer>
+        <input type="text" placeholder="Search" />
+        <i>
+          <SearchIcon />
+        </i>
+      </SearchContainer>
     </Nav>
   );
 };
@@ -17,9 +24,37 @@ export default Navbar;
 const Nav = styled.nav`
   border: 2px solid magenta;
   width: 100%;
-  height: 50px;
+  height: 55px;
+  display: flex;
+  align-items: center;
 `;
 
 const LogoContainer = styled.div`
   width: 100px;
+`;
+
+const SearchContainer = styled.form`
+  border: none;
+  position: relative;
+  input {
+    color: var(--grey-500);
+    background-color: var(--grey-900);
+    padding-top: 8px;
+    padding-bottom: 8px;
+    padding-left: 2.5rem;
+    border: 1px solid var(--grey-600);
+    border-radius: 1rem;
+  }
+  input::placeholder {
+    color: var(--grey-500);
+  }
+  /* search icon */
+  i {
+    stroke: var(--grey-500);
+    display: block;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-45%);
+    left: 15px;
+  }
 `;

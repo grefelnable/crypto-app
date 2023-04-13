9f98112306c0226627c31f0a8cc743b4c39db699
Grefel Nable <felnable@gmail.com>
2023-04-13 02:16:14 -0400

    Change name for slice for theme

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import theme from "../theme/slice";
+import theme from "./theme/themeSlice";
 
 export const store = configureStore({
   reducer: {

7256cd54071fcf9cdcbb13a1298024a4e44d3660
Grefel Nable <felnable@gmail.com>
2023-04-13 01:38:33 -0400

    Configure redus in Main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App";
 import "normalize.css";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import store from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );

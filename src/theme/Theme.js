2f660e8995927199ebd215d31f0456475d31b11e
Grefel Nable <felnable@gmail.com>
2023-04-14 17:55:14 -0400

    Add background variant to use on search component

--- src/theme/Theme.js
+++ src/theme/Theme.js
@@ -2,6 +2,7 @@ export const lightTheme = {
   body: "var(--grey-100)",
   text: "var(--grey-900)",
   background: "var(--grey-100)",
+  backgroundVariant: "var(--grey-200)",
   toggleBtn: "var(--grey-200)",
 };
 
@@ -9,5 +10,6 @@ export const darkTheme = {
   body: "var(--grey-900)",
   text: "var(--grey-100)",
   background: "var(--grey-900)",
+  backgroundVariant: "var(--grey-800)",
   toggleBtn: "var(--grey-700)",
 };

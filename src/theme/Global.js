7342f8dabb54cbfea731e9a61ebec6c7b3546f25
Grefel Nable <felnable@gmail.com>
2023-04-14 17:54:32 -0400

    Move transition to root

--- src/theme/Global.js
+++ src/theme/Global.js
@@ -7,7 +7,6 @@ export const GlobalStyles = createGlobalStyle`
  body{
   background: ${({ theme }) => theme.body};
   color: ${({ theme }) => theme.text};
-  // to make smooth transition on theme change
-  transition:var(--transition);
+  
  }
 `;

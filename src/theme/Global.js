8473c3e24bd51a75c44134fdcc2e34566f83e3e0
Grefel Nable <felnable@gmail.com>
2023-04-14 17:42:56 -0400

    Set transition as root to apply on every element
    In this way when theme changes, it will be a smooth transition

--- src/theme/Global.js
+++ src/theme/Global.js
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
+*{
+ transition:var(--transition);
+}
  body{
   background: ${({ theme }) => theme.body};
   color: ${({ theme }) => theme.text};

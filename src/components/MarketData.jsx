ac3f1f0c93908141cf846767adbb45284f01f6e3
Grefel Nable <felnable@gmail.com>
2023-04-17 02:22:31 -0400

    Add percentage bar on btc and eth

--- src/components/MarketData.jsx
+++ src/components/MarketData.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from "react";
 import { formatCompactNumber } from "../utils/FormatNumber";
 import styled from "styled-components";
+import ProgressBar from "./progress-bar.component";
 
 const MarketData = () => {
-  const [data, setData] = useState(0);
+  const [totalMarketCap, setTotalMarketCap] = useState(0);
   const [coins, setCoins] = useState(0);
   const [exchange, setExchange] = useState(0);
+  const [totalVolume, setTotalVolume] = useState(0);
+  const [btcMarketCapPercentage, setBtcMarketCapPercentage] = useState(0);
+  const [ethMarketCapPercentage, setEthMarketCapPercentage] = useState(0);
 
   // fetch data from https://api.coingecko.com/api/v3/global
   useEffect(() => {
@@ -13,25 +17,41 @@ const MarketData = () => {
       const response = await fetch("https://api.coingecko.com/api/v3/global");
       const items = await response.json();
       console.log(items);
-      setData(items.data.total_market_cap.usd);
       setCoins(items.data.active_cryptocurrencies);
       setExchange(items.data.markets);
+      setTotalMarketCap(items.data.total_market_cap.usd);
+      setTotalVolume(items.data.total_volume.usd);
+      setBtcMarketCapPercentage(
+        Math.floor(items.data.market_cap_percentage.btc)
+      );
+      setEthMarketCapPercentage(
+        Math.floor(items.data.market_cap_percentage.eth)
+      );
     };
     fetchMarketData().catch(console.error);
   }, []);
-  console.log(data);
 
   return (
     <Container>
       <li>Coins: {coins}</li>
       <li>Exchange: {exchange}</li>
-      <li>•${formatCompactNumber(data)}</li>
-      <li>Volume:</li>
-      <li>Bitcoin</li>
-      <li>Ethereum</li>
+      <li>•${formatCompactNumber(totalMarketCap)}</li>
+      <li>•${formatCompactNumber(totalVolume)}</li>
+      <li>
+        BTC: {btcMarketCapPercentage}%{" "}
+        <ProgressBar percentage={btcMarketCapPercentage} />
+      </li>
+      <li>
+        ETH: {ethMarketCapPercentage}%{" "}
+        <ProgressBar percentage={ethMarketCapPercentage} />
+      </li>
     </Container>
   );
 };
 export default MarketData;
 
-const Container = styled.ul``;
+const Container = styled.ul`
+  margin: 0 auto;
+  border: 1px solid magenta;
+  background: ${({ theme }) => theme.navbarBackground};
+`;
---
+++ src/components/progress-bar.component.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const ProgressBar = (props) => {
+  const { percentage } = props;
+  const fillerWidth = {
+    width: `${percentage}%`,
+  };
+  return (
+    <Container>
+      <Filler style={fillerWidth}></Filler>
+    </Container>
+  );
+};
+export default ProgressBar;
+
+const Container = styled.div`
+  height: 13px;
+  width: 55px;
+  background-color: var(--blue);
+  border-radius: 1em;
+`;
+
+const Filler = styled.div`
+  height: 100%;
+  background-color: ${({ theme }) => theme.text};
+  border-radius: inherit;
+`;

e7f2df9e9fd3cfea6bb2e52b2586d28d16642073
Grefel Nable <felnable@gmail.com>
2023-04-28 16:52:15 -0400

    Refactor if statements by adding curly brackets

--- src/redux/theme/themeSlice.js
+++ src/redux/theme/themeSlice.js
@@ -3,10 +3,13 @@ import { createSlice } from "@reduxjs/toolkit";
 // Get theme settings from local storage
 const getTheme = () => {
   const theme = `${window?.localStorage?.getItem("theme")}`;
-  if (["light", "dark"].includes(theme)) return theme;
-
+  if (["light", "dark"].includes(theme)) {
+    return theme;
+  }
   const userMedia = window.matchMedia("(prefers-color-scheme: light)");
-  if (userMedia.matches) return "light";
+  if (userMedia.matches) {
+    return "light";
+  }
 
   return "dark";
 };
@@ -17,7 +20,7 @@ export const themeSlice = createSlice({
   name: "theme",
   initialState,
   reducers: {
-    toggleTheme: (state, action) => action.payload,
+    toggleTheme: (action) => action.payload,
   },
 });
 

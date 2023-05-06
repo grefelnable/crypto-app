8aaf8337e7d92646d667ac490e6f40293f80ae5b
Grefel Nable <felnable@gmail.com>
2023-05-06 05:03:05 -0400

    Refactor:style the button and dropdown selection

--- src/components/SetCurrency.jsx
+++ src/components/SetCurrency.jsx
@@ -2,59 +2,123 @@ import styled from "styled-components";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { changeCurrency } from "../redux/currencySlice";
-import { useEffect } from "react";
+import { ReactComponent as Arrow } from "../assets/arrow-icon.svg";
 
 const SetCurrency = () => {
   const currency = useSelector((store) => store.currency);
 
-  useEffect(() => {
-    console.log(currency);
-  }, [currency]);
   // initialize dispatch variable
   const dispatch = useDispatch();
 
   const [showOptions, setShowOptions] = useState(false);
-  // const [test, setTest] = useState(currency);
 
+  // currency options
   const currencies = ["usd", "cad", "eur", "gbp"];
 
   const handleClick = () => {
     setShowOptions(!showOptions);
   };
+
+  const handleChangeCurrency = (e) => {
+    dispatch(changeCurrency(e.target.value));
+    setShowOptions(!showOptions);
+    console.log("clicked", e.target.value);
+  };
   return (
-    <div>
+    <Container>
       <CurrencyBtn type="button" onClick={handleClick}>
-        {currency.symbol} {currency.name}
+        <span>{currency.symbol}</span> {currency.name} <ArrowIcon />
       </CurrencyBtn>
       {showOptions && (
         <CurrencyOptions>
           {currencies.map((currency, index) => {
             return (
-              <CurrencyOptionBtn
-                key={index}
-                type="button"
-                value={currency}
-                onClick={(e) => dispatch(changeCurrency(e.target.value))}
-              >
-                {currency}
-              </CurrencyOptionBtn>
+              <CurrencyOptionList key={index}>
+                <CurrencyOptionBtn
+                  key={index}
+                  type="button"
+                  value={currency}
+                  onClick={handleChangeCurrency}
+                >
+                  {currency}
+                </CurrencyOptionBtn>
+              </CurrencyOptionList>
             );
           })}
         </CurrencyOptions>
       )}
-    </div>
+    </Container>
   );
 };
 export default SetCurrency;
 
 // CSS
-const CurrencyBtn = styled.button`
-  cursor: pointer;
-  text-transform: uppercase;
+const Container = styled.div`
+  position: relative;
+
+  /* test */
+  @media screen and (max-width: 768px) {
+    font-size: 1.25rem;
+  }
 `;
 
-const CurrencyOptions = styled.div``;
+const CurrencyBtn = styled.button`
+  cursor: pointer;
+  height: 2.65em;
+  text-transform: uppercase;
+  box-shadow: var(--shadow-1);
+  border: none;
+  border-radius: 0.5em;
+  color: ${({ theme }) => theme.text};
+  background-color: ${({ theme }) => theme.toggleBtn};
+  padding: 7px 10px;
+  transition: var(--transition);
+
+  span {
+    color: ${({ theme }) => theme.chartBorderColor};
+    background-color: ${({ theme }) => theme.navbarBackground};
+    padding: 4px 8px;
+    border-radius: 50%;
+    margin-right: 0.5em;
+  }
+  /* media query for small screens */
+  @media screen and (max-width: 768px) {
+    height: inherit;
+  }
+`;
+
+const CurrencyOptions = styled.ul`
+  border: 1px solid var(--grey-400);
+  border-radius: var(--borderRadius);
+  position: absolute;
+  top: -9.5px;
+  left: 30px;
+  padding: 2px 3px;
+  z-index: 10;
+  background: ${({ theme }) => theme.toggleBtn};
+`;
+
+const CurrencyOptionList = styled.li`
+  padding: 0 0.75em;
+  border-radius: var(--borderRadius);
+  transition: var(--transition);
+  &:hover {
+    background: var(--blue);
+  }
+`;
 
 const CurrencyOptionBtn = styled.button`
   cursor: pointer;
+  text-transform: uppercase;
+  color: ${({ theme }) => theme.text};
+  background: none;
+  border: none;
+`;
+
+const ArrowIcon = styled(Arrow)`
+  stroke: var(--green);
+  fill: var(--green);
+  margin-bottom: 0.1875em;
+  margin-right: 0.25em;
+  transform: rotate(180deg);
 `;

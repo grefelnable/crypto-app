151255214e74dfd153dad0627863d79e6e9cbb3f
Grefel Nable <felnable@gmail.com>
2023-05-11 07:00:55 -0400

    Setup Redux thunk to fetch coin data

--- src/redux/coinSlice.js
+++ src/redux/coinSlice.js
@@ -1,8 +1,9 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 // for production only; delete after
 import faker from "../faker";
-const initialState = faker;
+// const initialState = faker;
 //
 
 // get user currency setting from local storage
@@ -18,11 +19,21 @@ const getCurrency = () => {
 };
 const currency = getCurrency();
 
-console.log(currency);
-
 const BASE_URL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}&order=market_cap_desc&per_page=50&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d`;
 
-console.log(BASE_URL);
+// Initial state for coins data
+const initialState = {
+  coins: [],
+  status: "idle",
+  error: "",
+};
+
+// Fetch coins from API
+export const fetchCoins = createAsyncThunk("coins/fetchCoins", async () => {
+  const response = await axios.get(BASE_URL);
+  console.log(response);
+  return response?.data;
+});
 
 // Sort coin name alphabetically
 const handleSortName = () => {
@@ -32,12 +43,31 @@ const handleSortName = () => {
 };
 
 export const coinSlice = createSlice({
-  name: "coinData",
+  name: "coins",
   initialState,
   reducers: {
     sortName: (state) => (state = handleSortName()),
   },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchCoins.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchCoins.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.coins = state.coins.concat(action.payload);
+      })
+      .addCase(fetchCoins.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
 });
+
+// Selectors
+export const selectAllCoins = (state) => state.coins.coins;
+export const getCoinsStatus = (state) => state.coins.status;
+export const getCoinsError = (state) => state.coins.error;
 
 export const { sortName } = coinSlice.actions;
 

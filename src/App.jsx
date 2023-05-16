0a76c32ba28c82a9ad722da7677986282aef95d3
Grefel Nable <felnable@gmail.com>
2023-05-16 07:28:52 -0400

    Add SkeletonTheme that wraps the entire app
    Add color to be dynamic when user select a theme either dark or light

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Portfolio from "./pages/Portfolio";
 import { useEffect } from "react";
 import { fetchCoins } from "./redux/coinSlice";
 import CoinInformation from "./pages/CoinInformation";
+import { SkeletonTheme } from "react-loading-skeleton";
 
 function App() {
   // Get theme from store
@@ -37,18 +38,25 @@ function App() {
   return (
     <ThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
       <>
-        <GlobalStyles />
-        <Routes>
-          <Route path="/" element={<SharedLayout />}>
-            <Route index element={<Home />} />
-            <Route path="/coin" element={<CoinInformation />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-          </Route>
-          <Route path="*" element={<Error />} />
-        </Routes>
+        <SkeletonTheme
+          baseColor={theme === "dark" ? "#1e293b" : "#e2e8f0"}
+          highlightColor={theme === "dark" ? "#f1f5f9" : "#0f172a"}
+        >
+          <GlobalStyles />
+          <Routes>
+            <Route path="/" element={<SharedLayout />}>
+              <Route index element={<Home />} />
+              <Route path="/coin" element={<CoinInformation />} />
+              <Route path="/portfolio" element={<Portfolio />} />
+            </Route>
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </SkeletonTheme>
       </>
     </ThemeProvider>
   );
 }
 
 export default App;
+
+// Skeleton theme colors if "dark" or "light" theme
